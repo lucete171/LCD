6f4122260a9bc85b59641d4a4e353bd522d92652
lucete171 <80573129+lucete171@users.noreply.github.com>
2023-06-26 22:08:25 +0900

    Update tb_sm.v

--- Computation/tb_sm.v
+++ Computation/tb_sm.v
@@ -1,8 +1,29 @@
-//testbench of computation_1
+`timescale 1ns/1ns
 
-//timescale `timescale 1ns/1ns
+//testbench of computation of processing element(PE)
 module tb_sm;
-  PE_module PE_1(.A(), .B(), 
+  reg CLK;
+  reg RESET;
+  
+  reg [7:0] A_IN, B_IN;
+  
+  wire [7:0] OUTPUT, A_OUT, B_OUT;
+  
+  PE_module PE_1(.a(A_IN), .b(B_IN), .clk(CLK), .rst(RESET), .a_out(A_OUT), .b_out(B_OUT), .out(OUTPUT));
+  
+  initial
+  begin
+    RESET = 1'b0;
+    A_IN = 8'd13;
+    B_IN = 8'd11;
+  end
+  
+  initial
+  begin
+    #200 //delay of PE
+    A_IN = 8'd1;
+    B_IN = 8'd3;
+  end
   
   
 endmodule
