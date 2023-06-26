56f44e79e6b585b56a18b07602d3f4d1bd48baa6
lucete171 <80573129+lucete171@users.noreply.github.com>
2023-06-26 21:25:23 +0900

    Update PE_module.v

--- Computation/PE_module.v
+++ Computation/PE_module.v
@@ -12,6 +12,12 @@ module PE_module(a, b, clk, rst, a_out, b_out, out);
   output reg[7:0] b_out;
   output reg[7:0] out;
   
+  reg cout;
+  
+  reg [7:0] multi_out;
+  reg [7:0] sum = 8'b00000000;
+  reg tmp;
+  
   wire[7:0] a_bar;
   wire[7:0] b_bar;
   
@@ -20,14 +26,13 @@ module PE_module(a, b, clk, rst, a_out, b_out, out);
   d_flip_flop_behavioral_module out_2(.d(b), .clk(clk), .q(b_out), .q_bar(b_bar));
   
   //multiplier with and gate
+  eight_multi MUT(.a(a), .b(b), .out(multi_out));
   
-  
-  
-  //adder with full adder
-  
-  
+  //adder with eight bit adder
+  eight_add_module ADD(.a(mult_out), .b(sum), .sum(sum), .cout(tmp));
   
   //accumulator with full adder
+  //eight_bit_register
   
 endmodule
   
