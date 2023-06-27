bb47ce11b7ce6651836a948ddea23cd125fbe70a
lucete171 <80573129+lucete171@users.noreply.github.com>
2023-06-27 10:39:27 +0900

    Update tb_sm.v

--- Computation/tb_sm.v
+++ Computation/tb_sm.v
@@ -7,13 +7,23 @@ module tb_sm;
   
   reg [7:0] A_IN, B_IN;
   
+  //PE MODULE
   wire [7:0] OUTPUT, A_OUT, B_OUT;
   
-  PE_module PE_1(.a(A_IN), .b(B_IN), .clk(CLK), .rst(RESET), .a_out(A_OUT), .b_out(B_OUT), .out(OUTPUT));
+  //EIGHT ADD
+  reg [7:0] OUTPUT_ADD;
+  reg COUT_ADD;
+  
+  //EIGHT_MUL
+  reg [7:0] OUTPUT_MUL;
+  
+  //PE_module PE_1(.a(A_IN), .b(B_IN), .clk(CLK), .rst(RESET), .a_out(A_OUT), .b_out(B_OUT), .out(OUTPUT));
+  eight_bit_add_module ADD_TEST(.a(A_IN), .b(B_IN), .sum(OUTPUT_ADD), .cout(COUT_ADD));
+  
+  eight_bit_multi_module MUL_TEST(.a(A_IN), .b(B_IN), .out(OUTPUT_MUL));
   
   initial
   begin
-    OUTPUT=1'b0;
     RESET = 1'b0;
     A_IN = 8'd13;
     B_IN = 8'd11;
