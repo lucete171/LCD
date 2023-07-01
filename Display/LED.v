58360b28bb2a1bc96fa6caca2049bee40dd55290
jimingggi <137754602+jimingggi@users.noreply.github.com>
2023-07-01 13:25:40 +0900

    Add files via upload

--- Display/LED.v
+++ Display/LED.v
@@ -70,17 +70,17 @@ module seven_seg_display(
     always @(*)
     begin
         case(LED_BCD)
-        4'b0000 : seg_data = 7'b0111111; //0
-        4'b0001 : seg_data = 7'b0000110; //1
-        4'b0010 : seg_data = 7'b1011011; //2
-        4'b0011 : seg_data = 7'b1001111; //3
-        4'b0100 : seg_data = 7'b1100110; //4
-        4'b0101 : seg_data = 7'b1101101; //5
-        4'b0110 : seg_data = 7'b1111101; //6
-        4'b0111 : seg_data = 7'b0100111; //7
+        4'b0000 : seg_data = 7'b1111110; //0
+        4'b0001 : seg_data = 7'b0110000; //1
+        4'b0010 : seg_data = 7'b1101101; //2
+        4'b0011 : seg_data = 7'b1111001; //3
+        4'b0100 : seg_data = 7'b0110011; //4
+        4'b0101 : seg_data = 7'b1011011; //5
+        4'b0110 : seg_data = 7'b1011111; //6
+        4'b0111 : seg_data = 7'b1110010; //7
         4'b1000 : seg_data = 7'b1111111; //8
-        4'b1001 : seg_data = 7'b1101111; //9
-        default : seg_data = 7'b0111111; //0
+        4'b1001 : seg_data = 7'b1111011; //9
+        default : seg_data = 7'b1111110; //0
         endcase
     end
     
